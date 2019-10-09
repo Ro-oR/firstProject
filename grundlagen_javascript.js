b14fbdc2156e009272f4a38d16b3f64a8299bf21
unknown <waeschephilipp@gmail.com>
2019-10-09 12:12:15 +0200

    Aufgaben 1+2

---
+++ grundlagen_javascript.js
@@ -0,0 +1,27 @@
+console.log("Philipp"); //Aufgabe 1
+
+//Aufgabe 2
+const maxRating = 10;
+let overallRatings = 5;
+let currentRating = 8;
+
+console.log(maxRating + "\n" + overallRatings + "\n" + currentRating);
+
+overallRatings = 7;
+currentRating = 8.1;
+//maxRating = 9; TypeError: Assignment to constant variable.
+
+console.log(maxRating + "\n" + overallRatings + "\n" + currentRating);
+
+//Aufgabe 3
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Test? ", function(answer){
+    let input = answer;
+    console.log(input);
+    rl.close();
+});
