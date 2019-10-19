c86c6c8c5052854e169995389871fcc049969dcc
unknown <waeschephilipp@gmail.com>
2019-10-19 16:18:50 +0200

    .

--- test.js
+++ test.js
@@ -1,10 +1,31 @@
 const Song = require("./Song.js");
+const Playlist = require("./Playlist.js");
 
-var song1 = new Song("Testname");
+var song1 = new Song("Testname1");
+var song2 = new Song("Testname2");
+var song3 = new Song("Testname3");
+var song4 = new Song("Testname4");
+var song5 = new Song("Testname5");
+var pl = new Playlist();
 
-song1.rating += 10;
-song1.playSong();
-console.log(song1);
+song1.superVote();
+song1.vote();
+song2.rating = 51;
+song3.rating = 23;
+song4.rating = 41;
+song5.rating = 32;
+
+//console.log(song1);
+
+pl.addSong(song1);
+pl.addSong(song2);
+pl.addSong(song3);
+pl.addSong(song4);
+pl.addSong(song5);
+
+pl.printAll();
+pl.sort();
+pl.printAll();
 
 // // Diese Konstante wird für die Berechnung eines Kreisumfangs benötigt
 // const PI = 3.141592653589793;
